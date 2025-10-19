3d5abe225b6f10fa66ace1c069374eddd32225c3
SheonXJ <s87462830@gmail.com>
2025-10-20 00:43:17 +0800

    feat: add API for create loan data

--- src/routes/loan/index.ts
+++ src/routes/loan/index.ts
@@ -1,7 +1,40 @@
 import { FastifyInstance } from "fastify";
+import { Partial } from '@sinclair/typebox';
 
 export default async function loanRoutes(fastify: FastifyInstance) {
   fastify.get("/test", async () => {
     return "success";
   })
+
+fastify.post<{
+  Body: {
+    platform: string;
+    rate: number;
+    amount: number;
+  };
+}>("/", async (request, reply) => {
+  const { platform, rate, amount } = request.body;
+
+  const loanList = await fastify.prisma.loanHistory.create({
+    data: {
+      platform,
+      rate,
+      amount,
+    },
+  });
+
+  return reply.send(loanList);
+});
+
+  fastify.get<{
+    Querystring: Partial<{
+      email: string;
+      startAt: string;
+      endAt: string;
+    }>;
+  }>("/list", async (request, reply) => {
+    const loanList = await fastify.prisma.loanHistory.findMany({})
+    
+    return await reply.send(loanList)
+  });
 }
